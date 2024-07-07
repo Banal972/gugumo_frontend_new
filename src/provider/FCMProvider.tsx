717484b64a89f31111417f0e9bcb98ff92391532
Kim Ji Yu <spbabo97@naver.com>
2024-07-07 23:15:04 +0900

    :memo:
    코드 수정중

--- src/provider/FCMProvider.tsx
+++ src/provider/FCMProvider.tsx
@@ -1,38 +1,75 @@
 "use client"
-import { app, setTokenHanlder } from "@/lib/firebase";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { app } from "@/lib/firebase";
+import { setToken } from "@/lib/store/features/fcmtoken";
+import { useAppDispatch, useAppSelector } from "@/lib/store/hook";
+import { getMessaging, getToken } from "firebase/messaging";
+import { useSession } from "next-auth/react";
 import { useEffect } from "react";
+
+const subscribeFetch = async (session : any,fcmtoken : string)=>{
+
+  if(!session && !fcmtoken) return;
+
+  const response = await fetch('/back/api/v1/subscribe',{
+    method : "POST",
+    headers : {
+      "Authorization" : session?.accessToken
+    },
+    body : JSON.stringify({FCMToken : fcmtoken})
+  });
+
+  console.log(response);
+
+  if(!response.ok){
+    throw new Error('에러');
+  }
+
+  const data = await response.json();
+
+  console.log(data);
+
+}
+
+const onFcm = async (dispatch : any)=>{
+
+  const permission = await Notification.requestPermission();
+  if(permission !== "granted") return;
+
+  const messaging = getMessaging(app);
+
+  await getToken(messaging,{
+    vapidKey : process.env.NEXT_PUBLIC_FIREBASE_VAPIDKEY
+  })
+  .then(currentToken=>{
+    if(currentToken){
+      console.log(currentToken);
+      dispatch(setToken(currentToken));
+    }else{
+      console.log('오류');
+    }
+  })
+  .catch(err=>{
+    console.log(err);
+  });
+
+}
 
 export default function FCMProvider({children} : {children : React.ReactNode}) {
 
-  const onFcm = async ()=>{
-
-    const permission = await Notification.requestPermission();
-    if(permission !== "granted") return;
-
-    const messaging = getMessaging(app);
-
-    await getToken(messaging,{
-      vapidKey : process.env.NEXT_PUBLIC_FIREBASE_VAPIDKEY
-    })
-    .then(currentToken=>{
-      if(currentToken){
-        console.log(currentToken);
-      }else{
-        console.log('오류');
-      }
-    })
-    .catch(err=>{
-      console.log(err);
-    });
-
-  }
+  const {data : session} = useSession() as any;
+  const fcmtoken = useAppSelector((state) => state.fcmtoken);
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
-    onFcm();
-  }, [])
+    onFcm(dispatch);
+  }, [dispatch]);
+
+  useEffect(()=>{
+    subscribeFetch(session,fcmtoken);
+  },[session]);
 
   return (
     <>{children}</>
   )
+
 }
