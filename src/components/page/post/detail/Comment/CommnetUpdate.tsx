3348e3af1d641141fa514baddfc68aef7e950efe
Kim Ji Yu <spbabo97@naver.com>
2024-08-04 04:13:18 +0900

    :bug: 댓글 오류 수정
    댓글 모달창 수정
    댓글 수정하기 버튼 alert 창뜨지 않는 버그 수정

--- src/components/page/post/detail/Comment/CommnetUpdate.tsx
+++ src/components/page/post/detail/Comment/CommnetUpdate.tsx
@@ -1,51 +1,66 @@
+import Success from "@/components/Modal/Success";
 import { usePatchCommnet } from "@/hooks/useComment";
+import { open } from "@/lib/store/features/modals/modal";
+import { useAppDispatch } from "@/lib/store/hook";
 import { useSession } from "next-auth/react";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 
-export default function CommnetUpdate({setCommnetShow,commentId,content} : {setCommnetShow:any,commentId : number,content : string}) {
+export default function CommnetUpdate({
+  setCommnetShow,
+  commentId,
+  content,
+}: {
+  setCommnetShow: any;
+  commentId: number;
+  content: string;
+}) {
+  const dispatch = useAppDispatch();
+  const { data: session } = useSession();
+  const { register, handleSubmit, setValue } = useForm();
+  const { mutate: patchCommnet } = usePatchCommnet();
 
-    const {data : session} = useSession();
-    const {register,handleSubmit,setValue} = useForm();
-    const {mutate : patchCommnet} = usePatchCommnet();
+  const onSubmitHandler = async (data: any) => {
+    const { content } = data;
 
-    const onSubmitHandler = async (data : any)=>{
-        const {content} = data;
-
-        try {
-            patchCommnet({
-                comment_id : commentId,
-                session,
-                body : {content}
-            });
-            alert('수정이 완료 되었습니다.');
-            setCommnetShow({
-                commentId : 0,
-                type : "edit"
-            });
-        }
-        catch(err){
-            console.log(err);
-            setValue('content','');
-        }
-        finally {
-            setValue('content','');
-        }
-
+    try {
+      patchCommnet({
+        comment_id: commentId,
+        session,
+        body: { content },
+      });
+      dispatch(
+        open({
+          Component: Success,
+          props: { message: "수정이 완료 되었습니다." },
+        }),
+      );
+      setCommnetShow({
+        commentId: 0,
+        type: "edit",
+      });
+    } catch (err) {
+      console.log(err);
+      setValue("content", "");
+    } finally {
+      setValue("content", "");
     }
+  };
 
   return (
     <div className="flex-1 text-right">
-        <form onSubmit={handleSubmit(onSubmitHandler)}>
-            <textarea 
-                className="w-full resize-none block h-[68px] md:h-[108px] rounded md:rounded-xl bg-Surface p-3 md:px-4 md:py-5 text-sm md:text-base font-semibold placeholder:text-OnBackgroundGray border border-transparent outline-none focus:border-primary" 
-                placeholder="댓글을 입력해주세요."
-                {...register('content',{value : content})}
-            ></textarea>
-            <button 
-                type="submit" 
-                className="mt-2 md:mt-6 text-sm md:text-base font-semibold text-OnPrimary bg-primary py-2 px-4 rounded cursor-pointer"
-            >댓글 수정하기</button>
-        </form>
+      <form onSubmit={handleSubmit(onSubmitHandler)}>
+        <textarea
+          className="block h-[68px] w-full resize-none rounded border border-transparent bg-Surface p-3 text-sm font-semibold outline-none placeholder:text-OnBackgroundGray focus:border-primary md:h-[108px] md:rounded-xl md:px-4 md:py-5 md:text-base"
+          placeholder="댓글을 입력해주세요."
+          {...register("content", { value: content })}
+        ></textarea>
+        <button
+          type="submit"
+          className="mt-2 cursor-pointer rounded bg-primary px-4 py-2 text-sm font-semibold text-OnPrimary md:mt-6 md:text-base"
+        >
+          댓글 수정하기
+        </button>
+      </form>
     </div>
-  )
+  );
 }
--- src/components/page/post/detail/Comment/ReplyForm.tsx
+++ src/components/page/post/detail/Comment/ReplyForm.tsx
@@ -1,53 +1,72 @@
+import Alert from "@/components/Modal/Alert";
 import { usePostCommnet } from "@/hooks/useComment";
-import { useForm } from "react-hook-form"
+import { open } from "@/lib/store/features/modals/modal";
+import { useAppDispatch } from "@/lib/store/hook";
+import { useForm } from "react-hook-form";
 
-export default function ReplyForm({session,parentId,postId,setCommnetShow} : {session : any,parentId : number,postId : string,setCommnetShow : any}) {
+export default function ReplyForm({
+  session,
+  parentId,
+  postId,
+  setCommnetShow,
+}: {
+  session: any;
+  parentId: number;
+  postId: string;
+  setCommnetShow: any;
+}) {
+  const dispatch = useAppDispatch();
+  const { register, handleSubmit } = useForm();
+  const { mutate: postCommnet } = usePostCommnet();
 
-  const {register,handleSubmit} = useForm();
-  const {mutate : postCommnet} = usePostCommnet();
-  
-  const onSubmitHandler = (data : any)=>{
+  const onSubmitHandler = (data: any) => {
+    const { content } = data;
 
-    const {content} = data;
+    if (content === "") {
+      return dispatch(
+        open({
+          Component: Alert,
+          props: { message: "답글을 입력해야 합니다." },
+        }),
+      );
+    }
 
-    try{
+    try {
       postCommnet({
         session,
-        body : {
+        body: {
           postId,
           content,
-          parentCommentId : parentId
-        }
-      })
-    }
-    catch(err){
+          parentCommentId: parentId,
+        },
+      });
+    } catch (err) {
       console.log(err);
-    }
-    finally{
+    } finally {
       setCommnetShow({
-        commentId : 0,
-        type : "reply"
+        commentId: 0,
+        type: "reply",
       });
     }
-
-  }
+  };
 
   return (
-    <div className="ml-[15%] md:ml-[120px] mt-5">
-        <div className="flex-1 text-right">
-            <form onSubmit={handleSubmit(onSubmitHandler)}>
-                <textarea 
-                    className="w-full resize-none block h-[68px] md:h-[108px] rounded md:rounded-xl bg-Surface p-3 md:px-4 md:py-5 text-sm md:text-base font-semibold placeholder:text-OnBackgroundGray border border-transparent outline-none focus:border-primary" 
-                    placeholder="답글을 달아주세요"
-                    {...register('content',{maxLength: 1000, minLength : 1})}
-                />
-                <button 
-                  type="submit" 
-                  className="mt-2 md:mt-6 text-sm md:text-base font-semibold text-OnPrimary bg-primary py-2 px-4 rounded cursor-pointer"
-                >답글 등록하기</button>
-            </form>
-        </div>
+    <div className="ml-[15%] mt-5 md:ml-[120px]">
+      <div className="flex-1 text-right">
+        <form onSubmit={handleSubmit(onSubmitHandler)}>
+          <textarea
+            className="block h-[68px] w-full resize-none rounded border border-transparent bg-Surface p-3 text-sm font-semibold outline-none placeholder:text-OnBackgroundGray focus:border-primary md:h-[108px] md:rounded-xl md:px-4 md:py-5 md:text-base"
+            placeholder="답글을 달아주세요"
+            {...register("content", { maxLength: 1000, minLength: 1 })}
+          />
+          <button
+            type="submit"
+            className="mt-2 cursor-pointer rounded bg-primary px-4 py-2 text-sm font-semibold text-OnPrimary md:mt-6 md:text-base"
+          >
+            답글 등록하기
+          </button>
+        </form>
+      </div>
     </div>
-  )
-  
+  );
 }
