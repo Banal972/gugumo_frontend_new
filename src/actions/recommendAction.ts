c1c39e4d0f2f4856acaca7b40fa165adc5941733
Kim Ji Yu <spbabo97@naver.com>
2024-10-24 04:14:59 +0900

    refactor : 추천 게시물 server actions

---
+++ src/actions/recommendAction.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { authOptions } from "@/lib/authOptions";
+import { getServerSession } from "next-auth";
+
+const getRecommend = async () => {
+  const session = (await getServerSession(authOptions)) as any;
+
+  try {
+    const res = await fetch(`${process.env.API_URL}/api/v1/meeting/recommend`, {
+      headers: {
+        Authorization: session?.accessToken,
+      },
+    });
+    if (!res.ok) {
+      throw new Error("서버 에러");
+    }
+    return res.json();
+  } catch (err) {
+    throw new Error(err as string);
+  }
+};
+
+export default getRecommend;
--- src/components/Layout/Recommends/Recommends.tsx
+++ src/components/Layout/Recommends/Recommends.tsx
@@ -1,27 +1,13 @@
-import {
-  dehydrate,
-  HydrationBoundary,
-  QueryClient,
-} from "@tanstack/react-query";
 import Recommend from "./Recommend/Recommend";
-import { getServerSession } from "next-auth";
-import { recommendOptions } from "@/hooks/useRecommend";
 
 export default async function Recommends() {
-  const session = (await getServerSession()) as any;
-  const queryClinet = new QueryClient();
-
-  await queryClinet.prefetchQuery(recommendOptions({ session }));
-
   return (
     <div className="mt-16">
       <h3 className="text-lg font-bold text-primary md:text-2xl">
         추천 게시물 🎯
       </h3>
       <div className="mt-[22px] flex items-center gap-6 md:mt-11 xl:gap-11">
-        <HydrationBoundary state={dehydrate(queryClinet)}>
-          <Recommend />
-        </HydrationBoundary>
+        <Recommend />
       </div>
     </div>
   );
