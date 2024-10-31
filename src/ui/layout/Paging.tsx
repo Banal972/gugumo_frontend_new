e30a74f17972d6c4531aaa9276213995da49a8f7
Kim Ji Yu <spbabo97@naver.com>
2024-11-01 05:51:57 +0900

    feat : 페이징 기능 추가

--- src/ui/layout/Paging.tsx
+++ src/ui/layout/Paging.tsx
@@ -1,9 +1,27 @@
 'use client';
 
+import { Pageable } from '@/types/get.type';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
 const Paging = ({ pageable }: { pageable: Pageable }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const router = useRouter();
+  const activePage = searchParams.get('page') || '1';
+
   const currentGroup = Math.ceil(pageable.number / 5);
   const startPage = (currentGroup - 1) * 5 + 1;
   const endPage = Math.min(startPage + 5 - 1, pageable.totalPages);
+
+  const clickHandler = (page: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (page) {
+      params.set('page', page);
+    } else {
+      params.delete('page');
+    }
+    router.replace(`${pathname}?${params.toString()}`, { scroll: false });
+  };
 
   if (!pageable.empty) {
     return (
@@ -13,7 +31,8 @@ const Paging = ({ pageable }: { pageable: Pageable }) => {
           return (
             <button
               type="button"
-              className={`cursor-pointer ${pageable.number === page ? 'text-primary' : ''}`}
+              className={`cursor-pointer ${activePage === String(page) && 'text-primary'}`}
+              onClick={() => clickHandler(String(page))}
               key={page}
             >
               {page}
