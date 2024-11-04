17fc3b66150cae7fffd6a7b322d5f1fa46c03ddc
Kim Ji Yu <spbabo97@naver.com>
2024-11-05 05:01:14 +0900

    feat : zod init

--- src/app/(public)/find/page.tsx
+++ src/app/(public)/find/page.tsx
@@ -1,18 +1,26 @@
 'use client';
 
 import resetPwdAction from '@/actions/public/find/resetPwdAction';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
-type FormValues = {
-  email: string;
-};
+const schema = z.object({
+  email: z.string().min(1, { message: 'Required' }),
+});
 
 const FindPage = () => {
-  const { handleSubmit, register, setValue } = useForm<FormValues>();
+  const {
+    handleSubmit,
+    register,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
 
   const onSubmitHandler = handleSubmit(async (data) => {
     const { email } = data;
-    if (email === '') return alert('이메일을 입력해주세요.');
     const res = await resetPwdAction(email);
     setValue('email', '');
     if (!res.data) return window.alert('패스워드 이메일을 실패했습니다.');
@@ -26,7 +34,7 @@ const FindPage = () => {
           <dt className="text-xl font-semibold md:text-2xl">비밀번호 찾기</dt>
           <dd className="mt-6 break-keep text-sm leading-6">
             비밀번호를 재설정할 수 있는 이메일을 보내드립니다. <br />
-            발송된 이메일의 비밀번호 재설정은{' '}
+            발송된 이메일의 비밀번호 재설정은
             <span className="font-bold">10분 간</span> 유효합니다.
           </dd>
         </dl>
@@ -37,6 +45,7 @@ const FindPage = () => {
             placeholder="가입하신 이메일 주소를 입력해주세요."
             {...register('email')}
           />
+          {errors.email?.message && <p>{errors.email?.message}</p>}
           <button
             className="mx-auto mt-5 block h-12 w-[138px] cursor-pointer rounded-lg bg-primary text-base text-OnPrimary"
             type="submit"
